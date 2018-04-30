a1d7209895d96e161186bc655734f49e0086dd01
ubuntu <lopez.gaston.1996@gmail.com>
2018-04-30 17:01:07 -0300

    topologia OK

--- tp5/docker_tp5/bgp/del_default_gateway.sh
+++ tp5/docker_tp5/bgp/del_default_gateway.sh
@@ -3,11 +3,34 @@
 
 echo "Borro default gateways asignados por Docker"
 
-docker exec -it bgp_r1_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r1_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r2_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r2_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r3_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r3_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r4_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r4_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r9_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r9_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r5_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r5_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r5_1 ip -6 route del fe80::/64 dev eth2
+docker exec -it bgp_r6_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r6_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r6_1 ip -6 route del fe80::/64 dev eth2
+docker exec -it bgp_r7_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r7_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r7_1 ip -6 route del fe80::/64 dev eth2
+docker exec -it bgp_r8_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_r8_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_r8_1 ip -6 route del fe80::/64 dev eth2
 docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth0
 docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth2
+docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth3
+docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth4
 docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth0
 docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth2
+docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth3
+docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth4
+docker exec -it bgp_b3_1 ip -6 route del fe80::/64 dev eth0
+docker exec -it bgp_b3_1 ip -6 route del fe80::/64 dev eth1
+docker exec -it bgp_b3_1 ip -6 route del fe80::/64 dev eth2
