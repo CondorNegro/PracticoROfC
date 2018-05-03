d4e66a3c5e0e1afae94044afbfb42884899ea4c4
ubuntu <lopez.gaston.1996@gmail.com>
2018-05-03 09:03:32 -0300

    aa

--- tp4/docker_tp4/del_default_gateway.sh
+++ tp4/docker_tp4/del_default_gateway.sh
@@ -16,3 +16,8 @@ docker exec -it dockertp4_r8_1 ip -6 route del default
 docker exec -it dockertp4_r1_1 ip -6 route del default
 docker exec -it dockertp4_r2_1 ip -6 route del default
 
+echo "Asigno nuevo default gw"
+docker exec -it dockertp4_h11_1 ip -6 route add default via 2001:a115::5
+docker exec -it dockertp4_h13_1 ip -6 route add default via 2001:a137::7
+docker exec -it dockertp4_h12_1 ip -6 route add default via 2001:b612::6
+docker exec -it dockertp4_h14_1 ip -6 route add default via 2001:b814::8
