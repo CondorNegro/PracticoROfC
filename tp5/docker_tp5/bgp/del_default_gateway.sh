3b3c68ddf8a3e43f66042827409b8923d666bd22
ubuntu <lopez.gaston.1996@gmail.com>
2018-05-09 12:08:25 -0300

    ospf en tp5 anda

--- tp5/docker_tp5/bgp/del_default_gateway.sh
+++ tp5/docker_tp5/bgp/del_default_gateway.sh
@@ -3,38 +3,25 @@
 
 echo "Borro default gateways asignados por Docker"
 
-docker exec -it bgp_h11_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_h12_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_h13_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_h14_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r3_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r3_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r4_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r4_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r9_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r9_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r5_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r5_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r5_1 ip -6 route del fe80::/64 dev eth2
-docker exec -it bgp_r6_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r6_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r6_1 ip -6 route del fe80::/64 dev eth2
-docker exec -it bgp_r7_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r7_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r7_1 ip -6 route del fe80::/64 dev eth2
-docker exec -it bgp_r8_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_r8_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_r8_1 ip -6 route del fe80::/64 dev eth2
-docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth2
-docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth3
-docker exec -it bgp_b1_1 ip -6 route del fe80::/64 dev eth4
-docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth2
-docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth3
-docker exec -it bgp_b2_1 ip -6 route del fe80::/64 dev eth4
-docker exec -it bgp_b3_1 ip -6 route del fe80::/64 dev eth0
-docker exec -it bgp_b3_1 ip -6 route del fe80::/64 dev eth1
-docker exec -it bgp_b3_1 ip -6 route del fe80::/64 dev eth2
+docker exec -it bgp_h11_1 ip -6 route del default
+docker exec -it bgp_h12_1 ip -6 route del default
+docker exec -it bgp_h13_1 ip -6 route del default
+docker exec -it bgp_h14_1 ip -6 route del default
+docker exec -it bgp_r3_1 ip -6 route del default
+docker exec -it bgp_r4_1 ip -6 route del default
+docker exec -it bgp_r9_1 ip -6 route del default
+docker exec -it bgp_r5_1 ip -6 route del default
+docker exec -it bgp_r6_1 ip -6 route del default
+docker exec -it bgp_r7_1 ip -6 route del default
+docker exec -it bgp_r8_1 ip -6 route del default
+docker exec -it bgp_b1_1 ip -6 route del default
+docker exec -it bgp_b2_1 ip -6 route del default
+docker exec -it bgp_b3_1 ip -6 route del default
+
+echo "Asigno nuevo default gw"
+docker exec -it bgp_h11_1 ip -6 route add default via 2001:a115::5
+docker exec -it bgp_h13_1 ip -6 route add default via 2001:a137::7
+docker exec -it bgp_h12_1 ip -6 route add default via 2001:b612::6
+docker exec -it bgp_h14_1 ip -6 route add default via 2001:b814::8
+
+
